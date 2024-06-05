92f42bbd7ea70685865946631ebfb8a78c1bfcf7
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-05 15:02:36 -0600

    implement the complete globals.css file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
-import "./globals.css";
 import { cn } from "@/lib/utils";
+
+import "./globals.css";
 
 const IBMPlex = IBM_Plex_Sans({
   subsets: ["latin"],
