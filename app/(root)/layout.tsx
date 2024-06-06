84b6dab03c70d7e39548f6d91b3783fed803f912
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-05 23:26:18 -0600

    install the shadcn Sheet component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import MobileNav from "@/components/shared/MobileNav";
 import Sidebar from "@/components/shared/Sidebar";
 import React from "react";
 
@@ -5,6 +6,8 @@ const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
       <Sidebar />
+
+      <MobileNav />
 
       <div className="root-container">
         <div className="wrapper">{children}</div>
