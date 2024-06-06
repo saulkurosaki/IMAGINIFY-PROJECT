1eab820cb6e44ad8a09ad83fa32ded91ea26844e
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-05 22:51:23 -0600

    feat(Sidebar): implement the logo image within the sidebar

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Sidebar = () => {
   return (
     <aside className="sidebar">
-      <div className="flex size-full flex-col gap-4"></div>
+      <div className="flex size-full flex-col gap-4">
+        <Link href="/" className="sidebar-logo">
+          <Image
+            src="/assets/images/logo-text.svg"
+            alt="logo"
+            width={180}
+            height={28}
+          />
+        </Link>
+      </div>
     </aside>
   );
 };
