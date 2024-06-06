42fc8f4117bf66bde379cd6f51f42c07b6eea771
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-05 23:30:02 -0600

    feat(MobileNav): implement the logo image within the mobile navigation bar

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,5 +1,27 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import Link from "next/link";
+
 const MobileNav = () => {
-  return <div>MobileNav</div>;
+  return (
+    <header className="header">
+      <Link href="/" className="flex items-center gap-2 md:py-2">
+        <Image
+          src="/assets/images/logo-text.svg"
+          alt="logo"
+          width={180}
+          height={28}
+        />
+      </Link>
+    </header>
+  );
 };
 
 export default MobileNav;
