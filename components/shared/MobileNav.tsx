a5f104a5959e0d0a33a816e21b15480fda83517a
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-05 23:43:55 -0600

    feat(MobileNav): implement the on signed out display the login button

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -9,10 +9,11 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { navLinks } from "@/constants";
-import { SignedIn, UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { Button } from "../ui/button";
 
 const MobileNav = () => {
   const pathname = usePathname();
@@ -82,6 +83,12 @@ const MobileNav = () => {
             </SheetContent>
           </Sheet>
         </SignedIn>
+
+        <SignedOut>
+          <Button asChild className="button bg-purple-gradient bg-cover">
+            <Link href="sign-in">Login</Link>
+          </Button>
+        </SignedOut>
       </nav>
     </header>
   );
