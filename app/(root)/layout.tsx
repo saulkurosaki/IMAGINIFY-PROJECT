6528e43274a3a0b556b8d5a029b28bb31abf5b26
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-05 21:10:41 -0600

    feat(Home): implement the Home page component initial layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-const layout = () => {
-  return <div>layout</div>;
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="auth">
+      <div className="root-container">
+        <div className="wrapper">{children}</div>
+      </div>
+    </main>
+  );
 };
 
 export default layout;
