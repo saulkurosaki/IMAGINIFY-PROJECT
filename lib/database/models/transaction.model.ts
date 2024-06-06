ac2da1bc9c496a71057995acdffc974456936564
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-06 13:36:05 -0600

    feat(transaction.model.ts): implement the ITransaction interface

--- lib/database/models/transaction.model.ts
+++ lib/database/models/transaction.model.ts
@@ -1,13 +1,13 @@
 import { Schema, model, models } from "mongoose";
 
-// export interface ITransaction {
-//   createdAt?: Date;
-//   stripeId: string;
-//   amount: number;
-//   plan?: string;
-//   credits?: number;
-//   buyer?: string;
-// }
+export interface ITransaction {
+  createdAt?: Date;
+  stripeId: string;
+  amount: number;
+  plan?: string;
+  credits?: number;
+  buyer?: string;
+}
 
 const TransactionSchema = new Schema({
   createdAt: {
