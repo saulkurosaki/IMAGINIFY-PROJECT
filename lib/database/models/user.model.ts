de741d72475c86a06af3f1eada7457ab9ed7e843
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-06 13:32:39 -0600

    feat(user.model.ts): implement the mongoose User Model

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -47,6 +47,6 @@ const UserSchema = new Schema({
   },
 });
 
-// const User = models?.User || model("User", UserSchema);
+const User = models?.User || model("User", UserSchema);
 
-// export default User;
+export default User;
