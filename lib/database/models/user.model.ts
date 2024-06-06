9e155f110500e8a2e0ebb7c85e1db52bdf5960eb
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-06 13:32:22 -0600

    feat(user.model.ts): implement the complete UserSchema

---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,52 @@
+import { Schema, model, models } from "mongoose";
+
+// interface IUser {
+//   clerkId: string;
+//   email: string;
+//   username: string;
+//   photo: string;
+//   firstName?: string;
+//   lastName?: string;
+//   planId?: number;
+//   creditBalance?: number;
+// }
+
+const UserSchema = new Schema({
+  clerkId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  planId: {
+    type: Number,
+    default: 1,
+  },
+  creditBalance: {
+    type: Number,
+    default: 10,
+  },
+});
+
+// const User = models?.User || model("User", UserSchema);
+
+// export default User;
