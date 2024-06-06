5361eac866139e795c70c423657d4745fbee6497
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-05 23:02:00 -0600

    feat(Sidebar): implement the on signed in sidebar route links display

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,7 +1,14 @@
+"use client";
+
+import { navLinks } from "@/constants";
+import { SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
     <aside className="sidebar">
       <div className="flex size-full flex-col gap-4">
@@ -13,6 +20,38 @@ const Sidebar = () => {
             height={28}
           />
         </Link>
+
+        <nav className="sidebar-nav">
+          <SignedIn>
+            <ul className="sidebar-nav_elements">
+              {navLinks.map((link) => {
+                const isActive = link.route === pathname;
+
+                return (
+                  <li
+                    key={link.route}
+                    className={`sidebar-nav_element group ${
+                      isActive
+                        ? "bg-purple-gradient text-white"
+                        : "text-gray-700"
+                    }`}
+                  >
+                    <Link href={link.route} className="sidebar-link">
+                      <Image
+                        src={link.icon}
+                        alt="logo"
+                        width={24}
+                        height={24}
+                        className={`${isActive && "brightness-200"}`}
+                      />
+                      {link.label}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </SignedIn>
+        </nav>
       </div>
     </aside>
   );
