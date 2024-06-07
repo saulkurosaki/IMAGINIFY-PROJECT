ff41ed9105c2a7f4ae0c90015ffb6cd0349f2e11
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-06 19:56:16 -0600

    feat(Header): implement the Header component initial layout

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,8 @@
+import Header from "@/components/shared/Header";
 import React from "react";
 
 const AddTransformationTypePage = () => {
-  return <div>AddTransformationTypePage</div>;
+  return <Header />;
 };
 
 export default AddTransformationTypePage;
