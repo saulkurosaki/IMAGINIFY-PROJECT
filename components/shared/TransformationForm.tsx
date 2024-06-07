0a699bb7be894d66d00571c9bacc61acfae672f3
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-07 09:53:47 -0600

    feat(TransformationForm): implement the complete object to remove and object to recolor custom field when type is remove or recolor

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -78,6 +78,13 @@ const TransformationForm = ({
     onChangeField: (value: string) => void
   ) => {};
 
+  const onInputChangeHandler = (
+    fieldName: string,
+    value: string,
+    type: string,
+    onChangeField: (value: string) => void
+  ) => {};
+
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -116,11 +123,32 @@ const TransformationForm = ({
           />
         )}
 
-        {/* {(type === 'remove') || (type === 'recolor') && (
+        {(type === "remove" || type === "recolor") && (
           <div className="prompt-field">
-            <CustomField />
+            <CustomField
+              control={form.control}
+              name="prompt"
+              formLabel={
+                type === "remove" ? "Object to remove" : "Object to recolor"
+              }
+              className="w-full"
+              render={({ field }) => (
+                <Input
+                  value={field.value}
+                  className="input-field"
+                  onChange={(e) =>
+                    onInputChangeHandler(
+                      "prompt",
+                      e.target.value,
+                      type,
+                      field.onChange
+                    )
+                  }
+                />
+              )}
+            />
           </div>
-        )} */}
+        )}
       </form>
     </Form>
   );
