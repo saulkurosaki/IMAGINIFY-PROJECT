33ad3292c073f96c0f7ec5bad533c4491540ade3
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-07 13:49:21 -0600

    feat(MediaUploader): implement the onUploadSuccessHanlder and onUploadErrorHandler functions

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -1,9 +1,23 @@
 import { useToast } from "../ui/use-toast";
+import { CldUploadWidget } from "next-cloudinary";
 
-const MediaUploader = () => {
+const MediaUploader = ({}) => {
   const { toast } = useToast();
 
-  return <div>MediaUploader</div>;
+  const onUploadSuccessHandler = (result: any) => {};
+  const onUploadErrorHandler = () => {};
+
+  return (
+    <CldUploadWidget
+      uploadPreset="saul_imginify"
+      options={{
+        multiple: false,
+        resourceType: "image",
+      }}
+      onSuccess={onUploadSuccessHandler}
+      onError={onUploadErrorHandler}
+    ></CldUploadWidget>
+  );
 };
 
 export default MediaUploader;
