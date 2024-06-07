79794c0c77a817abfc300b15e51d25448873b22d
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-07 15:10:48 -0600

    feat(TransformedImage): implement the component container layout along as the download button when needed

--- components/shared/TransformedImage.tsx
+++ components/shared/TransformedImage.tsx
@@ -1,5 +1,35 @@
-const TransformedImage = () => {
-  return <div>TransformedImage</div>;
+import Image from "next/image";
+
+const TransformedImage = ({
+  image,
+  type,
+  title,
+  transformationConfig,
+  isTransforming,
+  setIsTransforming,
+  hasDownload = false,
+}: TransformedImageProps) => {
+  const downloadHandler = () => {};
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex-between">
+        <h3 className="h3-bold text-dark-600">Transformed</h3>
+
+        {hasDownload && (
+          <button className="download-btn" onClick={downloadHandler}>
+            <Image
+              src="/assets/icons/download.svg"
+              alt="Download"
+              width={24}
+              height={24}
+              className="pb-[6px]"
+            />
+          </button>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default TransformedImage;
