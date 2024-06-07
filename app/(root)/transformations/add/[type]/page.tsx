0d4004cc6d46a78da8952976c89eea6b5966365c
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-06 20:14:54 -0600

    feat(Header): implement the complete Header component as well as display the dynamic Header component title and subtitle based off the url params

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,8 +1,12 @@
 import Header from "@/components/shared/Header";
-import React from "react";
+import { transformationTypes } from "@/constants";
 
-const AddTransformationTypePage = () => {
-  return <Header />;
+const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+  const transformation = transformationTypes[type];
+
+  return (
+    <Header title={transformation.title} subtitle={transformation.subTitle} />
+  );
 };
 
 export default AddTransformationTypePage;
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,5 +1,12 @@
-const Header = () => {
-  return <div>Header</div>;
+const Header = ({ title, subtitle }: { title: string; subtitle?: string }) => {
+  return (
+    <>
+      <h2 className="h2-bold text-dark-600">
+        {title}
+        {subtitle && <p className="p-16-regular mt-4">{subtitle}</p>}
+      </h2>
+    </>
+  );
 };
 
 export default Header;
