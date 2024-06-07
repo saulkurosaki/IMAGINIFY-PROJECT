fc78c2664d662852bef279728f2f116f228889c8
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-07 14:21:04 -0600

    feat(MediaUploader): implement the layout when the image has been uploaded

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { useToast } from "../ui/use-toast";
-import { CldUploadWidget } from "next-cloudinary";
+import { CldImage, CldUploadWidget } from "next-cloudinary";
 import Image from "next/image";
+import { dataUrl, getImageSize } from "@/lib/utils";
+import { PlaceholderValue } from "next/dist/shared/lib/get-img-props";
 
 type MediaUploaderProps = {
   onValueChange: (value: string) => void;
@@ -53,7 +55,19 @@ const MediaUploader = ({
           <h3 className="h3-bold text-dark-600">Original</h3>
 
           {publicId ? (
-            <>HERE IS THE IMAGE</>
+            <>
+              <div className="cursor-pointer overflow-hidden rounded-[10px]">
+                <CldImage
+                  width={getImageSize(type, image, "width")}
+                  height={getImageSize(type, image, "height")}
+                  src={publicId}
+                  alt="image"
+                  sizes={"(max-width: 767px) 100vw, 50vw"}
+                  placeholder={dataUrl as PlaceholderValue}
+                  className="media-uploader_cldImage"
+                />
+              </div>
+            </>
           ) : (
             <div className="media-uploader_cta" onClick={() => open()}>
               <div className="media-uploader_cta-image">
