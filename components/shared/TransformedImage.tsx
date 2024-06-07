fbd336696794c36668743ba5210d04340d6965f4
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-07 15:19:44 -0600

    feat(TransformedImage): implement the complete TransformedImage component

--- components/shared/TransformedImage.tsx
+++ components/shared/TransformedImage.tsx
@@ -1,3 +1,6 @@
+import { dataUrl, debounce, getImageSize } from "@/lib/utils";
+import { CldImage } from "next-cloudinary";
+import { PlaceholderValue } from "next/dist/shared/lib/get-img-props";
 import Image from "next/image";
 
 const TransformedImage = ({
@@ -28,6 +31,42 @@ const TransformedImage = ({
           </button>
         )}
       </div>
+
+      {image?.publicId && transformationConfig ? (
+        <div className="relative">
+          <CldImage
+            width={getImageSize(type, image, "width")}
+            height={getImageSize(type, image, "height")}
+            src={image?.publicId}
+            alt={image.title}
+            sizes={"(max-width: 767px) 100vw, 50vw"}
+            placeholder={dataUrl as PlaceholderValue}
+            className="transformed-image"
+            onLoad={() => {
+              setIsTransforming && setIsTransforming(false);
+            }}
+            onError={() => {
+              debounce(() => {
+                setIsTransforming && setIsTransforming(false);
+              }, 8000);
+            }}
+            {...transformationConfig}
+          />
+
+          {isTransforming && (
+            <div className="transforming-loader">
+              <Image
+                src="/assets/icons/spinner.svg"
+                alt="Transforming"
+                width={50}
+                height={50}
+              />
+            </div>
+          )}
+        </div>
+      ) : (
+        <div className="transformed-placeholder">Transformed Image</div>
+      )}
     </div>
   );
 };
