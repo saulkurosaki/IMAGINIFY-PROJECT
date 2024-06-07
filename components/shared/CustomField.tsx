77ed8ef223f9c1faa5bff4e7c28ccee95edf8dbc
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-06 20:34:29 -0600

    feat(CustomField): implement the complete CustomField component

--- components/shared/CustomField.tsx
+++ components/shared/CustomField.tsx
@@ -1,5 +1,43 @@
-const CustomField = () => {
-  return <div>CustomField</div>;
+import React from "react";
+import { Control } from "react-hook-form";
+import { z } from "zod";
+
+import {
+  FormField,
+  FormItem,
+  FormControl,
+  FormMessage,
+  FormLabel,
+} from "../ui/form";
+
+import { formSchema } from "./TransformationForm";
+
+type CustomFieldProps = {
+  control: Control<z.infer<typeof formSchema>> | undefined;
+  render: (props: { field: any }) => React.ReactNode;
+  name: keyof z.infer<typeof formSchema>;
+  formLabel?: string;
+  className?: string;
 };
 
-export default CustomField;
+export const CustomField = ({
+  control,
+  render,
+  name,
+  formLabel,
+  className,
+}: CustomFieldProps) => {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className={className}>
+          {formLabel && <FormLabel>{formLabel}</FormLabel>}
+          <FormControl>{render({ field })}</FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
