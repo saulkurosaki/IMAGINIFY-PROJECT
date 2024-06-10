a90b799dd182c9e58cba668b1b7cd48f00b4badc
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-10 14:44:42 -0600

    feat(transaction.action.ts): implement the complete checkoutCredits function

---
+++ lib/actions/transaction.action.ts
@@ -0,0 +1,38 @@
+"use server";
+
+import { PUBLISHABLE_KEY } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import Stripe from "stripe";
+
+export const checkoutCredits = async (
+  transaction: CheckoutTransactionParams
+) => {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
+  const amount = Number(transaction.amount) * 100;
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price_data: {
+          currency: "usd",
+          unit_amount: amount,
+          product_data: {
+            name: transaction.plan,
+          },
+        },
+        quantity: 1,
+      },
+    ],
+    metadata: {
+      plan: transaction.plan,
+      credits: transaction.credits,
+      buyerId: transaction.buyerId,
+    },
+    mode: "payment",
+    success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
+    cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
+  });
+
+  redirect(session.url!);
+};
