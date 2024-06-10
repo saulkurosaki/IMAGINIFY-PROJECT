ce83042403ec191aa690112eded4eaba20ed085c
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-10 10:35:16 -0600

    feat(Home): implement the home section complete banner

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,15 @@ import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <>
+      <section className="home">
+        <h1 className="home-heading">
+          Unleash Your Creative Vision with Imaginify
+        </h1>
+      </section>
+    </>
+  );
 };
 
 export default Home;
