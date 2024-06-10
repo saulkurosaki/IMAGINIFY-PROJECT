d609e35fb4831f965adfd2f994d29e97be61f54c
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-10 11:43:32 -0600

    fix(TransformationForm): fix minor issues

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -230,6 +230,7 @@ const TransformationForm = ({
                 onValueChange={(value) =>
                   onSelectFieldHandler(value, field.onChange)
                 }
+                value={field.value}
               >
                 <SelectTrigger className="select-field">
                   <SelectValue placeholder="Select size" />
