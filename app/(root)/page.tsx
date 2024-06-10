40401e724bb3cf947df8fda121a933baf74233b9
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-10 12:16:14 -0600

    feat(Home): minor changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,20 +3,19 @@ import { navLinks } from "@/constants";
 import { getAllImages } from "@/lib/actions/image.actions";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 
 const Home = async ({ searchParams }: SearchParamProps) => {
   const page = Number(searchParams?.page) || 1;
   const searchQuery = (searchParams?.query as string) || "";
 
   const images = await getAllImages({ page, searchQuery });
+
   return (
     <>
       <section className="home">
         <h1 className="home-heading">
           Unleash Your Creative Vision with Imaginify
         </h1>
-
         <ul className="flex-center w-full gap-20">
           {navLinks.slice(1, 5).map((link) => (
             <Link
